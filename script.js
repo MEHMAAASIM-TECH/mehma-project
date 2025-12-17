dcd7aa3aaff03ebfb4446585a64c00a7c491c812
MEHMAAASIM-TECH <std_37907@iobm.edu.pk>
2025-12-17 12:15:15 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+function explore(){
+    alert("Welcome! Explore local tourism places.");
+}
+
+function details(){
+    alert("Details will be added later.");
+}
+
+function loginMsg(){
+    alert("Login successful (Demo only)");
+}
+
+function goBack(){
+    window.history.back();
+}
